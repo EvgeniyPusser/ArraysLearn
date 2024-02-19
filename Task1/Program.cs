3b2bdd1b57a789fc8f07f215403be0a6a9092dc7
Evgeniy Pusser <evgeniipusser88@gmail.com>
2024-02-19 15:11:18 +0300

    arrays from video

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿//Console.Write($"\t1\t2\t3");
+Console.Write("Put size of Array  ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int [,] table1 = new int[n, n];
+
+for(int i = 0; i < n; i++){
+    Console.Write($"{i + 1}");
+    for (int j = 0; j < n; j++){
+      //Console.Write($"\t{table1[i, j]}");
+        table1[i, j] = (i + 1) * (j + 1);
+    }
+    Console.WriteLine();
+
+}
+
+for(int i = 0; i < n; i++){
+    Console.Write($"{i + 1}");
+    for (int j = 0; j < n; j++){
+      Console.Write($"\t{table1[i, j]}");
+       // table1[i, j] = (i + 1) * (j + 1);
+    }
+    Console.WriteLine();
+
+}
